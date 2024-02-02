04a5eeaa1d8f791e21553b052e4154a3241199fd
sayoi341 <me@sayoi341.moe>
2024-02-02 16:13:30 +0900

    ♻️ repoからmyへ変更

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@my/ui/card";
+import { Code } from "@my/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@my/ui/button";
 
 function Gradient({
   conic,
