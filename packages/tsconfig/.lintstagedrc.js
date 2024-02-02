f8a49a5c88a7d8a81d348903e715ca26d7f6f043
sayoi341 <me@sayoi341.moe>
2024-02-02 17:21:35 +0900

    ✨ tsconfigかんせ

---
+++ packages/tsconfig/.lintstagedrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+    '**/*.{js,ts,json}': (/** @type {string[]} */ filenames) => `bun eslint --fix ${filenames.join(' --fix ')}`,
+    '**/*.{js,ts,json}': (/** @type {string[]} */ filenames) =>
+        `bun prettier --check ${filenames.join(' --check ')} --write ${filenames.join(' --write ')}`,
+};
