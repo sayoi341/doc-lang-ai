8e9cceab39061069fe13f6703f0dd9b18bd07990
sayoi341 <me@sayoi341.moe>
2024-02-02 16:50:25 +0900

    eslintの設定

---
+++ packages/eslint/eslint-config-nextjs/.lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "**/*.{js,ts,json}": (/** @type {string[]} */ filenames) =>
+        `bun eslint --fix ${filenames.join(" --fix ")}`,
+    "**/*.{js,ts,json}": (/** @type {string[]} */ filenames) =>
+        `bun prettier --check ${filenames.join(
+            " --check "
+        )} --write ${filenames.join(" --write ")}`,
+};
