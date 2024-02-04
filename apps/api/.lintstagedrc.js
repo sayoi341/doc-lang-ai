a7624b5242b0de86d69ca24c398362e80c176f33
sayoi341 <me@sayoi341.moe>
2024-02-05 02:12:52 +0900

    いつもの奴ら

---
+++ apps/api/.lintstagedrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  '**/*.{js,ts,tsx,json}': (/** @type {string[]} */ filenames) => `bun eslint --fix ${filenames.join(' --fix ')}`,
+  '**/*.{js,ts,tsx,json}': (/** @type {string[]} */ filenames) =>
+    `bun prettier --check ${filenames.join(' --check ')} --write ${filenames.join(' --write ')}`,
+  '**/*.{css,scss}': (/** @type {string[]} */ filenames) => `bun stylelint --fix ${filenames.join(' --fix ')}`,
+};
