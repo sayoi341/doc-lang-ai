8a845ab252c7478f2e36aad6003ada168948c19d
sayoi341 <me@sayoi341.moe>
2024-02-07 22:44:49 +0900

    new style

--- apps/mobile/src/app/camera/index.tsx
+++ apps/mobile/src/app/camera/index.tsx
@@ -1,12 +1,21 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useRouter } from 'expo-router';
-import React from 'react';
-import { Text, TouchableOpacity, View, Button } from 'react-native';
-
-import useCamera from '~/hooks/useCamera';
+import { AutoFocus, Camera, CameraType } from 'expo-camera';
+import { router } from 'expo-router';
+import React, { useState } from 'react';
+import { TouchableOpacity, View, Text } from 'react-native';
+import { useAtom } from 'jotai';
+import PictureAtom from '~/stores/picture.atom';
 
 const App = () => {
-  const { setCamera, permission, reqestPermission, picture, takePicture } = useCamera();
+  const [camera, setCamera] = useState<Camera>();
+  const [permission, reqestPermission] = Camera.useCameraPermissions();
+  const [picture, setPicture] = useAtom(PictureAtom);
+
+  const takePicture = async (callback: () => void) => {
+    setPicture(undefined);
+    const { base64 } = await camera!.takePictureAsync({ base64: true, quality: 0 });
+    setPicture(base64 ?? '');
+    callback();
+  };
 
   if (!permission) {
     return <View />;
@@ -14,12 +23,16 @@ const App = () => {
 
   if (!permission.granted) {
     return (
-      <TouchableOpacity
-        className=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        onPress={() => reqestPermission()}
-      >
-        <Text className="font-medium text-white ">カメラアクセスを要求する！！！！！</Text>
-      </TouchableOpacity>
+      <View className="flex-1 items-center justify-center">
+        <TouchableOpacity
+          className=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          onPress={() => {
+            reqestPermission();
+          }}
+        >
+          <Text className="font-medium text-white ">カメラアクセスを要求する！！！！！</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 
@@ -28,12 +41,13 @@ const App = () => {
       <Camera
         className="h-5/6 w-full"
         type={CameraType.back}
+        autoFocus={AutoFocus.on}
         ref={(ref) => {
           setCamera(ref!);
         }}
       ></Camera>
       <View className="h-1/6 w-full items-center bg-black">
-        <TouchableOpacity className="mt-4 h-16 w-16 rounded-full bg-white" onPress={() => takePicture('/camera/prompt')}></TouchableOpacity>
+        <TouchableOpacity className="mt-4 h-16 w-16 rounded-full bg-white" onPress={() => takePicture(() => router.push('/camera/prompt'))} />
       </View>
     </View>
   );
--- apps/mobile/src/app/camera/prompt.tsx
+++ apps/mobile/src/app/camera/prompt.tsx
@@ -1,40 +1,89 @@
+import { Picker } from '@react-native-picker/picker';
 import { Buffer } from 'buffer';
 import { router } from 'expo-router';
+import React, { useState } from 'react';
+import { Text, TouchableOpacity, View, ActivityIndicator } from 'react-native';
 import { useAtom } from 'jotai';
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import PictureAtom from '~/stores/picture.atom';
-import TransratedText from '~/stores/transratedText.atom';
+import Icon from '~/components/Icon';
 import fetcher from '~/utils/fetcher';
+import PictureAtom from '~/stores/picture.atom';
+import languages from '~/constants/language';
 
 const App = () => {
   const [picture, setPicture] = useAtom(PictureAtom);
-  const [transratedText, setTransratedText] = useAtom(TransratedText);
+  const [isLoading, setIsLoading] = useState(false);
+  const [transratedText, setTransratedText] = useState('');
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
+  const fetch = async (lang: string) => {
+    setIsLoading(true);
+    try {
+      const res = await fetcher.chat.postChat({
+        headers: {
+          Authorization: `Basic ${Buffer.from(process.env.EXPO_PUBLIC_USER + ':' + process.env.EXPO_PUBLIC_PASS).toString('base64')}`,
+        },
+        body: {
+          image: picture,
+          language: lang,
+        },
+      });
+      setIsLoading(false);
+      console.log(res);
+      setTransratedText(res.body.kwargs.content);
+    } catch (e) {
+      setIsLoading(false);
+      console.error(e);
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <View className="flex flex-col gap-10 items-center justify-center my-auto">
+        <Text className="text-4xl font-bold">翻訳中</Text>
+        <Icon name="filetext1" color="black" size={128} />
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (transratedText === '') {
+    return (
+      <View className="flex flex-col gap-10 items-center justify-center my-auto">
+        <View>
+          <Text className="text-4xl font-bold">文章を翻訳しますか？</Text>
+        </View>
+        <View className="flex flex-col text-black">
+          <Text className="text-xl">翻訳する言語を選択して下さい</Text>
+          <Picker className="" selectedValue={selectedLanguage} onValueChange={(itemValue) => setSelectedLanguage(itemValue)}>
+            {languages.map((language) => (
+              <Picker.Item label={language} value={language} key={language} />
+            ))}
+          </Picker>
+        </View>
+        <View className="flex flex-row gap-5 justify-between">
+          <TouchableOpacity className="w-32 items-center bg-white rounded-lg px-5 py-2.5 me-2 mb-2" onPress={() => router.push('/camera/')}>
+            <Text className="text-gray-900 text-base font-medium">再撮影</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className=" w-32 items-center bg-blue-700 rounded-lg px-5 py-2.5 me-2 mb-2" onPress={() => fetch(selectedLanguage!)}>
+            <Text className="text-white text-base font-medium">翻訳する</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
   return (
     <View className="flex-1 items-center justify-center">
-      <Text className="font-medium">文章を翻訳しますか？</Text>
+      <Text className="font-medium">翻訳結果</Text>
+
+      <Text className="font-medium">{transratedText}</Text>
       <TouchableOpacity
         className=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        onPress={async () => {
-          fetcher.chat
-            .postChat({
-              headers: {
-                Authorization: `Basic ${Buffer.from(process.env.EXPO_PUBLIC_USER + ':' + process.env.EXPO_PUBLIC_PASS).toString('base64')}`,
-              },
-              body: {
-                image: picture,
-                language: 'japanese',
-              },
-            })
-            .then((res) => {
-              console.log(res.body.kwargs.content);
-              setTransratedText(res.body.kwargs.content);
-              router.push('/camera/result');
-            });
+        onPress={() => {
+          router.push('/home/');
         }}
       >
-        <Text className="text-white font-medium">翻訳する!!!!</Text>
+        <Text className="text-white font-medium">戻る</Text>
       </TouchableOpacity>
     </View>
   );
